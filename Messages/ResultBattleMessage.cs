8cd4266697a88669fc5886397b337ac06ea22a91
Valentyn <forwow30@gmail.com>
2024-12-19 21:10:22 +0200

    Upd messages

--- Messages/ResultBattleMessage.cs
+++ Messages/ResultBattleMessage.cs
@@ -3,5 +3,7 @@
     public class ResultBattleMessage 
     {
         public bool isWin { get; set; }
+        
+        [NonSerialized] public string UserId;
     }
 }
--- Messages/StartBattleMessage.cs
+++ Messages/StartBattleMessage.cs
@@ -5,5 +5,7 @@ namespace ServerTCP.Gaming.Messages
     public class StartBattleMessage
     {
         public BattleCommand[] BattleActions { get; set; }
+        
+        [NonSerialized] public string UserId;
     }
 }
