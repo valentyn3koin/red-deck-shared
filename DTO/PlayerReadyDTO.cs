ec03bfa3f04bc003b7b385956bc181f8c8b6c93e
Valentyn <forwow30@gmail.com>
2024-12-20 12:04:37 +0200

    Add playerReady DTO

---
+++ DTO/PlayerReadyDTO.cs
@@ -0,0 +1,11 @@
+﻿using ServerTCP.Gaming.Infos;
+
+namespace ServerTCP.Gaming.DTO
+{
+    public class PlayerReadyDTO
+    {
+        public string UserId { get; set; }
+        public HeroInfo HeroInfo { get; set; }
+        public bool IsReady { get; set; }
+    }
+}
