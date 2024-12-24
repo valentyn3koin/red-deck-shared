c6fc2e039e475d36d213d94920ee8d9212524aa7
Valentyn <forwow30@gmail.com>
2024-12-24 14:49:08 +0200

    Upd battleCoommandExtentionh

--- Utils/BattleCommandExtention.cs
+++ Utils/BattleCommandExtention.cs
@@ -1,26 +1,27 @@
 ﻿using Newtonsoft.Json;
 using ServerTCP.Gaming.Actions;
 
-namespace ServerTCP.Submodule.Utils;
-
-public static class BattleCommandExtensions
+namespace ServerTCP.Submodule.Utils
 {
-    public static BattleCommand CreateAction<T>(this BattleCommand battleCommand, ActionBattleType actionType, T data)
+    public static class BattleCommandExtensions
     {
-        string jsonData = JsonConvert.SerializeObject(data);
-        return new BattleCommand()
+        public static BattleCommand CreateAction<T>(this BattleCommand battleCommand, ActionBattleType actionType, T data)
         {
-            ActionType = actionType,
-            AdditionalData = jsonData
-        };
-    }
-    
-    public static T? TakeAction<T>(this BattleCommand battleCommand)
-    {
-        if (string.IsNullOrEmpty(battleCommand.AdditionalData))
-        {
-            return default;
+            string jsonData = JsonConvert.SerializeObject(data);
+            return new BattleCommand()
+            {
+                ActionType = actionType,
+                AdditionalData = jsonData
+            };
         }
-        return JsonConvert.DeserializeObject<T>(battleCommand.AdditionalData);
+    
+        public static T? TakeAction<T>(this BattleCommand battleCommand)
+        {
+            if (string.IsNullOrEmpty(battleCommand.AdditionalData))
+            {
+                return default;
+            }
+            return JsonConvert.DeserializeObject<T>(battleCommand.AdditionalData);
+        }
     }
 }
