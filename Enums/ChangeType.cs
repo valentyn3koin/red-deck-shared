238be36bd53c5664084c57e1ae952050593de3ca
Taras Samaryn <samarintaras@gmail.com>
2024-12-25 22:36:32 +0800

    update sub

--- Enums/ChangeType.cs
+++ Enums/ChangeType.cs
@@ -3,24 +3,29 @@
     public enum ChangeType
     {
         None = 0,
+        
         Remove = 1,
         Add = 2,
+        
         Freeze = 3,
         Unfreeze = 4,
-        Buff = 5,
-        AddGold = 6,
+        
         Summon = 7,
         Draw = 8,
+        
         AddStatus = 9,
+        
         AddAbility = 10,
-        AddAttack = 11,
         AddAttackPermanently = 12,
-        AddHpAndAttack = 13,
-        AddHp = 14,
         AddHpMultiplication = 15,
-        AddHpAndAttackMultiplication2 = 16,
         AddHpPermanently = 17,
         GainSelfHp = 18,
+        AddGold = 6,
+        Buff = 5,
+        AddAttack = 11,
+        AddHpAndAttack = 13,
+        AddHp = 14,
+        AddHpAndAttackMultiplication2 = 16,
         GainSelfHpMultiplication2 = 19,
         StickyEffect = 20,
         EnrageAddDamageEffect = 21,
