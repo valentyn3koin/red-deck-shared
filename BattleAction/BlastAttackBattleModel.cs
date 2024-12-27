f33c5496fc8ee713f1b16d563680564c5042c3cc
Valentyn <forwow30@gmail.com>
2024-12-27 16:39:34 +0200

    add balst type

--- BattleAction/BlastAttackBattleModel.cs
+++ BattleAction/BlastAttackBattleModel.cs
@@ -4,5 +4,12 @@
     {
         public string AttackerUid {get; set;}
         public string[] DefenderUid {get; set;}
+        public BlastType BlastType {get; set;}
+    }
+
+    public enum BlastType
+    {
+        Fire,
+        Frost
     }
 }
