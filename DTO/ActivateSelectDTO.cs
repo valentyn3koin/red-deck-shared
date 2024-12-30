8c9ff6f0d8e3051bed0b87f0a9d23031e72af0c0
Taras Samaryn <samarintaras@gmail.com>
2024-12-30 20:35:49 +0800

    update dto select

--- DTO/ActivateSelectDTO.cs
+++ DTO/ActivateSelectDTO.cs
@@ -6,6 +6,7 @@ namespace ServerTCP.Gaming.DTO
 {
     public class ActivateSelectDTO
     {
+        public string Caster { get; set; }
         public List<string> CardsForChoose { get; set; } = new();
         [NonSerialized] public string UserId;
     }
