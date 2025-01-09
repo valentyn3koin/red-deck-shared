b111fdfbad29fc370c1666e3faca047f195b358f
Valentyn <forwow30@gmail.com>
2025-01-09 16:04:49 +0200

    Add server info

---
+++ Infos/ServerInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerTCP.Gaming.Infos
+{
+    public class ServerInfo
+    {
+        public string ServerId { get; set; }
+        public string ServerEnvironment { get; set; }
+        public string ServerName { get; set; }
+        public int ConnectedPlayers { get; set; }
+        public string Version { get; set; }
+    }
+}
